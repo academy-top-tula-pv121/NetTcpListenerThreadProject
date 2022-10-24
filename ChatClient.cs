ea574c4fd68b05c0577b8d78d297d7a863912ab4
maxefimov <efimov.top.academy@gmail.com>
2022-10-24 21:31:16 +0300

    Добавьте файлы проекта.

---
+++ ChatClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace NetTcpListenerThreadProject
+{
+    internal class ChatClient
+    {
+        TcpClient client;
+        public ChatClient(TcpClient client)
+        {
+            this.client = client;
+        }
+        public void Chat()
+        {
+            NetworkStream stream = null!;
+            try
+            {
+                stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+
+                while (true)
+                {
+                    StringBuilder strClient = new StringBuilder();
+                    int bufferSize = 0;
+                    do
+                    {
+                        bufferSize = stream.Read(buffer, 0, buffer.Length);
+                        strClient.Append(Encoding.Default.GetString(buffer, 0, bufferSize));
+                    } while (stream.DataAvailable);
+                    Console.WriteLine($"Client message {DateTime.Now.ToShortDateString()}: {strClient.ToString()}");
+
+                    string strListener = "Your message: " + strClient.ToString();
+                    buffer = Encoding.Default.GetBytes(strListener);
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(stream != null)
+                    stream.Close();
+                if(client != null)
+                    client.Close();
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+namespace NetTcpListenerThreadProject
+{
+    internal class Program
+    {
+        const int port = 10001;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new(IPAddress.Loopback, port);
+                listener.Start();
+                Console.WriteLine("Server is Listener...");
+
+                while(true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ChatClient clientObj = new(client);
+
+                    Thread threadClient = new(clientObj.Chat);
+                    threadClient.Start();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if(listener != null)
+                    listener.Stop();
+            }
+        }
+    }
+}
